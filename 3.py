75d15502f6226cedb5c8e355e7a8b3b4fcf46c48
Sarath5441 <51222749+Sarath5441@users.noreply.github.com>
2019-06-02 19:52:04 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+ch=input()
+
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+    print("Vowel")
+elif(ch == '!' or ch == '@' or ch == '#' or ch == '$' or ch == '%' or ch == '^' or ch == '&' or ch == '*'
+        or ch == '(' or ch == ')'):
+    print("invalid")
+else:
+    print("consonant")
