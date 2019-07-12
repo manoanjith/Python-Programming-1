d83cf4bad49b8d2823b5d4e8267ae6384c932beb
Sarath5441 <51222749+Sarath5441@users.noreply.github.com>
2019-07-12 02:30:05 -0700

    Update even.py

--- even.py
+++ even.py
@@ -1,6 +1,6 @@
-M,N=map(int,input().split())
-for a in range(M+1,N):
-    if(a%2==0):
-        print(a,end=" ")
+a,b=map(int,input().split())
+for m in range(M+1,N):
+    if(m%2==0):
+        print(m,end=" ")
     else:
         continue
