a5178aa57abe037ae0b11a672f476cb53e523bf5
manoanjith <52821003+manoanjith@users.noreply.github.com>
2019-07-12 05:25:10 -0700

    Update set2.5.py

--- set2.5.py
+++ set2.5.py
@@ -1,6 +1,6 @@
-b,c=map(int,input().split())
-for a in range(b+1,c):
-    if a%2==0:
-        print(a,end=" ")
+g,h=map(int,input().split())
+for b in range(g+1,h):
+    if b%2==0:
+        print(b,end=" ")
     else:
         pass
